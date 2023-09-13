ae3c9804b58922ac720a4ba1d0be0f81a911045c
leonardoraupp <leonardoraupprodrigues@hotmail.com>
2023-09-13 19:44:42 -0300

    arquivo de config com o banco de dados

---
+++ db_connection.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'safe_city'
+})
+
+connection.connect((error) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log('Connected to the database');
+    }
+  });
+
+  module.exports = { connection }
+  
