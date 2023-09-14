a04c7a7d2bc3930474e50751214cafec32f488b0
leonardoraupp <leonardoraupprodrigues@hotmail.com>
2023-09-13 21:27:57 -0300

    organizando o projeto

--- src/services/api_cep.js
+++ src/services/api_cep.js
@@ -1,9 +1,8 @@
-const request = require('request')
-
-const cep = '93212260'
+const request = require('request');
+const cep = '93212260';
 const uri = `https://viacep.com.br/ws/${cep}/json/`;
 
 request.get(uri, (error, response, body) => {
     if (error) console.log(error)
     console.log(body)
-})
+});
