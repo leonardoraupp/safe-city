8f6752ee5d3408a9cb7d07a0de6852c1c44ba9a4
Leonardo <leonardoraupprodrigues@hotmail.com>
2024-06-14 20:24:02 -0300

    adicionando sequelize  e configarações

--- src/api/db.js
+++ src/api/db.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'root',
-    database: 'safe_city'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
 })
 
 connection.connect((error) => {
