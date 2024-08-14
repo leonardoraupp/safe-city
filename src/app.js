5412ae3fef80904b5305231f5d5d2a3b82305e0e
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2024-08-13 22:31:56 -0300

    implementation for testing on Render deployment

--- src/app.js
+++ src/app.js
@@ -11,9 +11,14 @@ const usersRoutes = require("./api/components/user/routes")
 const app = express()
 const PORT = process.env.PORT || 3000
 const NODE_ENV = process.env.NODE_ENV || 'development'
+const DB_HOST = process.env.DB_HOST
+const DB_PORT = process.env.DB_PORT
+const DB_USER = process.env.DB_USER
+const DB_PWD = process.env.DB_PWD
+const DB_NAME = process.env.DB_NAME
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT} and ENV ${NODE_ENV}`)
+    console.log(`Server is running on PORT ${PORT} and ENV ${NODE_ENV}: DB_HOST= ${DB_HOST}, DB_PORT= ${DB_PORT}, DB_USER= ${DB_USER}, DB_PWD= ${DB_PWD}, DB_NAME= ${DB_NAME}`)
 })
 
 // Adding headers to our requests.
