eda26eaf59abb72dc70459e5d8c3f7716b37b3ff
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2024-08-13 22:49:23 -0300

    postgreeSQL configuration

--- src/db.js
+++ src/db.js
@@ -1,20 +1,43 @@
 require("dotenv").config();
 const mysql = require('mysql2');
+const { Client } = require('pg');
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PWD,
-  database: process.env.DB_NAME
-})
+let connection;
 
-connection.connect((error) => {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('Connected to the database');
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  connection = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
 
-  }
-});
+  connection.connect((error) => {
+    if (error) {
+      console.error('PostgreSQL connection error', error.stack);
+    } else {
+      console.log('Connected to the PostgreSQL database');
+    }
+  });
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME
+  });
 
-module.exports = { connection }
+  connection.connect((error) => {
+    if (error) {
+      console.error('MySQL connection error', error);
+    } else {
+      console.log('Connected to the MySQL database');
+    }
+  });
+}
+
+module.exports = { connection };
